5fc784b721b1101286d3a1e24ff75048b83fa7b6
Smidge101 <laurengoff99883@gmail.com>
2025-09-11 21:36:58 -0500

    More to go!!

--- bot.py
+++ bot.py
@@ -1,5 +1,19 @@
+##More stuff I want the bot to do:
+#1. If someone were to say $slither to @user the snake will respond with a slithering gif
+#2. If someone needs help with the commands, they can use $help. The bot will responds with a list of commands
+#4. If someone says $bite @user, the bot will respond with either a snake biting gif or a nuzzel gif
+
+
+
 #import the discord library
 import discord
+import json
+import requests
+
+def get_meme():
+  response = requests.get('https://meme-api.com/gimme/snakememes') #You can change the subreddit to any subreddit you want
+  json_data = json.loads(response.text)
+  return json_data['url']
 
 #Created a class that inherits from discord.Client
 class MyClient(discord.Client):
@@ -11,8 +25,17 @@ class MyClient(discord.Client):
   async def on_message(self, message):
     if message.author == self.user:
       return
-    if message.content.startswith('$hello'):
-      await message.channel.send('Ssssss!(Hello World!)')
+    
+    #Check if the message starts with $meme, thus it prints out a "funny" snake meme
+    if message.content.startswith('$meme'):
+      await message.channel.send(get_meme())
+    #When the user says $hello the bot responds with its own little hello message
+    elif message.content.startswith('$hello'):
+      await message.channel.send('Ssss!(Hello!)')
+    #If you tell the bot I love you it will respond back with a response and a cute snake image
+    elif message.content.startswith('$Love you!'):
+      await message.channel.send("Sss!(I love you too!)")
+      await message.channel.send("https://i.pinimg.com/736x/8f/5a/0b/8f5a0b9787f745cfe0fe8974d860435c.jpg")
 
 #setting for what the bot can access in the server. Assigned the default behavior for the bot 
 # thus we have to make the bot message become true
